b97ea68fc2441ef8cff5fab007f94957aab4c3e0
XiaoZiDan <916976364>
2021-11-10 21:17:19 +0800

    v1.0.0

---
+++ test/mytest01/src/test/java/com/gzf/mytest/Mytest01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gzf.mytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mytest01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
